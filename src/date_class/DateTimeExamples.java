5048b389fb2764eb8c8a32b3c3ea8308100f2786
fmt <1105896230@qq.com>
2016-08-13 19:36:20 +0800

    加入新的api

--- src/date_class/DateTimeExamples.java
+++ src/date_class/DateTimeExamples.java
@@ -4,6 +4,11 @@ package date_class;
  * Created by Administrator on 2016/8/13 0013.
  */
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoField;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -12,7 +17,7 @@ import java.util.Date;
  */
 public class DateTimeExamples {
     public static void main(String[] args) {
-        test1();
+        test4();
     }
 
 
@@ -31,5 +36,52 @@ public class DateTimeExamples {
         Calendar calendar = Calendar.getInstance();
         calendar.set(2014, Calendar.FEBRUARY, 18);
         System.out.println(calendar);
+    }
+
+    /**
+     * java8引入新的时间日期和时间库
+     */
+    private static void test2() {
+        LocalDate date = LocalDate.of(2014, 3, 18);
+        int year = date.getYear();
+        Month month = date.getMonth();
+        int dayOfMonth = date.getDayOfMonth();
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        int i = date.lengthOfMonth();
+        boolean leapYear = date.isLeapYear();
+        System.out.println(year);
+        System.out.println(month);
+
+        System.out.println(dayOfMonth);
+
+        System.out.println(dayOfWeek);
+        System.out.println(i);
+        System.out.println(leapYear);
+
+
+    }
+
+    /**
+     * localTime ,来描述一天的是时间
+     */
+    private static void test3() {
+        LocalTime time = LocalTime.of(13, 45, 20); // 13:45:20
+        int hour = time.getHour(); // 13
+        int minute = time.getMinute(); // 45
+        int second = time.getSecond(); // 20
+        System.out.println(time);
+    }
+
+    /**
+     * ChronoField 来读取LocalDate里面的数据
+     */
+    private static void test4() {
+        LocalDate date = LocalDate.of(2014, 3, 18);
+        int y = date.get(ChronoField.YEAR);
+        int m = date.get(ChronoField.MONTH_OF_YEAR);
+        int d = date.get(ChronoField.DAY_OF_MONTH);
+        System.out.println(y);
+        System.out.println(m);
+        System.out.println(d);
     }
 }
