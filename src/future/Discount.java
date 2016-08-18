fcf39c0ac5c444b344aeb3c34d1c98cbf417d750
fmt <1105896230@qq.com>
2016-08-18 23:13:36 +0800

    异步方法

---
+++ src/future/Discount.java
@@ -0,0 +1,29 @@
+package future;
+
+import static future.Util.delay;
+import static future.Util.format;
+
+/**
+ * Created by Administrator on 2016/8/18 0018.
+ */
+public class Discount {
+
+    public enum Code {
+        NONE(0), SILVER(5), GOLD(10), PLATINUM(15), DIAMOND(20);
+
+        private final int percentage;
+
+        Code(int percentage) {
+            this.percentage = percentage;
+        }
+    }
+
+    public static String applyDiscount(Quote quote) {
+        return quote.getShopName() + " price is " + Discount.apply(quote.getPrice(), quote.getDiscountCode());
+    }
+
+    private static double apply(double price, Code code) {
+        delay();
+        return format(price * (100 - code.percentage) / 100);
+    }
+}
